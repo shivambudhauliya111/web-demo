1111f7209eaccb0018e533187ee833ba8291824e
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2025-08-22 16:45:38 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,14 @@
+// Last updated: 8/22/2025, 4:45:38 PM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       for(int i = 0;i<nums.length;i++){
+        if(nums[i]>=target ){
+           
+            return i;
+
+        }
+       
+       }
+       return nums.length; 
+    }
+}
