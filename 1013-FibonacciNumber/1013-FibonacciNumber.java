6f45019fd3109149353448537c6e3ea37410ba1e
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2025-08-22 16:44:48 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 8/22/2025, 4:44:48 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+
+        }else if(n==1){
+            return 1;
+        }
+        int firstTerm = 0;
+        int secondTerm = 1;
+        for(int i =1; i<=n;i++){
+            int thirdTerm = firstTerm+secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+        }
+        return firstTerm;
+        
+    }
+}
