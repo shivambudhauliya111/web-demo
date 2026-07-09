94fef118e857eae419b315b8e5e687926fe4ac4e
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2026-07-09 15:33:23 +0530

    Create 1221-SplitaStringinBalancedStrings

---
+++ 1221-SplitaStringinBalancedStrings/version/java/1221-SplitaStringinBalancedStrings_20260709_153324.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 15:33:24
+1class Solution {
+2    public int balancedStringSplit(String s) {
+3        int balance = 0;
+4        int count = 0;
+5        for (char ch : s.toCharArray()) {
+6            if (ch == 'R') {
+7                balance++;
+8            } else {
+9                balance--;
+10            }
+11            if (balance == 0) {
+12                count++;
+13            }
+14        }
+15        return count;
+16    }
+17}
