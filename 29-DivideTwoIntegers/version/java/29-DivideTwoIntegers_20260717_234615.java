1be632b2150d50639b9e73ddd19feafb5f662e96
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2026-07-17 23:46:15 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/version/java/29-DivideTwoIntegers_20260717_234615.java
@@ -0,0 +1,10 @@
+// Last updated: 17/07/2026, 23:46:15
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        if(dividend == -2147483648&&divisor == -1){
+4            return  2147483647;
+5        }
+6      int x=dividend/divisor;
+7      return x;    
+8    }
+9}
