60b90f93df54af7e91b866e75891c1e52b9293bd
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2026-07-18 23:09:23 +0530

    Create

---
+++ problem-1784396365176/version/java/problem-1784396365176_20260718_230925.java
@@ -0,0 +1,16 @@
+// Last updated: 18/07/2026, 23:09:25
+1class Solution {
+2    public boolean isMonotonic(int[] nums) {
+3        boolean increasing = true;
+4        boolean decreasing = true;
+5        for (int i = 1; i < nums.length; i++) {
+6            if (nums[i] > nums[i - 1]) {
+7                decreasing = false;
+8            }
+9            if (nums[i] < nums[i - 1]) {
+10                increasing = false;
+11            }
+12        }
+13        return increasing || decreasing;
+14    }
+15}
