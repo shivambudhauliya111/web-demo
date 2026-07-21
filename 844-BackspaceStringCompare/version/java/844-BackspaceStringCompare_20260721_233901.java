59b975826a3038f6ae81e5a28774a74c27dbeb73
Shivam Budhauliya <shivambudhauliya111@gmail.com>
2026-07-21 23:39:01 +0530

    Create 844-BackspaceStringCompare

---
+++ 844-BackspaceStringCompare/version/java/844-BackspaceStringCompare_20260721_233901.java
@@ -0,0 +1,17 @@
+// Last updated: 21/07/2026, 23:39:01
+1class Solution {
+2    public boolean backspaceCompare(String s, String t) {
+3        return build(s).equals(build(t));
+4    }
+5    private String build(String str) {
+6        StringBuilder sb = new StringBuilder();
+7        for (char c : str.toCharArray()) {
+8            if (c != '#') {
+9                sb.append(c);
+10            } else if (sb.length() > 0) {
+11                sb.deleteCharAt(sb.length() - 1);
+12            }
+13        }
+14        return sb.toString();
+15    }
+16}
